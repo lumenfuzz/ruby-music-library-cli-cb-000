80adbb16d09e37c51316dd87bb7ee2f6811f1856
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-09 19:15:35 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -24,6 +24,14 @@ class MusicLibraryController
       case input
       when "list songs"
         self.list_songs
+      when "list artists"
+        self.list_artists
+      when "list genres"
+        self.list_genres
+      when "list artist"
+        self.list_songs_by_artist
+      when "list genre"
+        self.list_songs_by_genre
       end
     end
   end
