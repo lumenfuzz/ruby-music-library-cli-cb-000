8410af971b2410c48788164ae6ec54ba62c30b90
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-09 16:23:59 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,16 @@
 module Findable
 
   def find_by_name(name)
-    self.all.detect{|a| a.name == name}
+  self.all.each do |song|
+    return song if song.name == name
   end
+  return nil
+end
+
+def find_or_create_by_name(name)
+  song = self.find_by_name(name)
+  return song if song != nil
+  return self.create(name) if song == nil
+end
 
 end
